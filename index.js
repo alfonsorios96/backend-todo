bf4765f268784669919fe241b0d6c190b1e02c25
Alfonso Rios <malforime@gmail.com>
2019-11-22 21:25:20 -0600

    Create CDN static for webComponents

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const colors = require('colors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
 dotenv.config();
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -16,6 +17,22 @@ const services = require('./services');
 const middlewares = require('./middlewares');
 
 app.post('/auth', services.authenticate.auth);
+
+const catalogRoute = express.Router();
+
+catalogRoute.use(middlewares.authenticate.authorize);
+
+catalogRoute.get('/', (request, response) => {
+    response.status(200).json({
+        uid: 'eg721ge71ge7121',
+        name: 'guts-component',
+        url: `${process.env.DOMAIN}/catalog/guts-component.js`
+    });
+});
+
+catalogRoute.use('/cdn', express.static(path.join(__dirname, 'components')));
+
+app.use('/catalog', catalogRoute);
 
 app.listen(PORT);
 
--- services/auth.js
+++ services/auth.js
@@ -2,8 +2,11 @@ const jwt = require('jsonwebtoken');
 
 const auth = (request, response) => {
     const user = request.body;
-    const users = ['test-lana','admin-lana'];
-    const match = users.includes(user.username);
+    const users = [{
+        username: 'malforime',
+        password: '1234'
+    }];
+    const match = users.some(_user => user.username === _user.username && user.password === _user.password);
     if(match) {
         const token = jwt.sign(user.username, process.env.SECRET_KEY);
         response.status(200).json({
